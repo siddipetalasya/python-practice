1ba5da9b075ce1658dd35ffafb7893dabc52f7c6
siddipetalasya <siddipetalasya@gmail.com>
2026-01-03 10:34:24 +0530

    Update and rename def even_odd(num)1.py to Functions/without argument and with return type/def even_odd(num)2.py

---
+++ Functions/without argument and with return type/def even_odd(num)2.py
@@ -0,0 +1,11 @@
+
+def even_odd():
+    num = int(input("Enter a number: "))
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+# Function call
+result = even_odd()
+print("Number is", result)
--- def even_odd(num)1.py
+++
@@ -1,8 +0,0 @@
-def even_odd(num):
-    if num % 2 == 0:
-        print(num, "is Even")
-    else:
-        print(num, "is Odd")
-
-# Function call
-even_odd(8)
